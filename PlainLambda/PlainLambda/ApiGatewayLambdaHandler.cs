1aeb36a7753b6964636e64a8f1888f4a86e94d91
rodrigo.ibanez <rodrigo.ibanez@rokt.com>
2019-03-25 23:37:34 +1100

    Added the first and simplest lambda function in c# (netcoreapp 2.1) and the cloudformation template.

---
+++ PlainLambda/PlainLambda/ApiGatewayLambdaHandler.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace PlainLambda
+{
+    public class ApiGatewayLambdaHandler
+    {
+        public ApiGatewayResponse Handle()
+        {
+            return new ApiGatewayResponse
+            {
+                Body = "My first plain lambda",
+                StatusCode = 200
+            };
+        }
+    }
+
+    public class ApiGatewayResponse
+    {
+        public string Body { get; set; }
+        public int StatusCode { get; set; }
+    }
+}
